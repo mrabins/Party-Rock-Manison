cdb647da7e9b3e1997fd1fab2f6887cbeddcf391
Mark Rabins <markrabins@gmail.com>
2016-11-22 16:07:43 -0800

    Handle contraints, and imageQueue data

--- Party Rock Manison/PartyTableViewCell.swift
+++ Party Rock Manison/PartyTableViewCell.swift
@@ -13,22 +13,35 @@ class PartyTableViewCell: UITableViewCell {
     @IBOutlet weak var videoPreviewImage: UIImageView!
     @IBOutlet weak var videoTitle: UILabel!
     
-
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
         
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
     
     func updateUI(partyRock: PartyRock) {
         videoTitle.text = partyRock.videoTitle
-        // TODO: set image from URL
+        
+        let url = URL(string: partyRock.imageURL)!
+        
+        DispatchQueue.global().async {
+            do {
+                let data = try Data(contentsOf: url)
+                DispatchQueue.global().sync {
+                    self.videoPreviewImage.image = UIImage(data: data)
+                }
+            } catch {
+                
+                // handle the error
+            }
+            
+        }
+        
     }
-
 }
--- Party Rock Manison/ViewController.swift
+++ Party Rock Manison/ViewController.swift
@@ -14,21 +14,37 @@ class ViewController: UIViewController {
     
     var partyRocks = [PartyRock]()
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        let p1 = PartyRock(imageURL: "https://i.ytimg.com/vi/ev4bY1SkZLg/hqdefault.jpg?custom=true&w=120&h=90&jpg444=true&jpgq=90&sp=68&sigh=wu8nQ4Kj5WblFEVyVw_ZovfCLMg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/ev4bY1SkZLg?list=PLtM4rw7sjPKUe0YW6Fmrp2FSU7RxkX-JA\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Lights Out")
+        
+        let p2 = PartyRock(imageURL: "https://i.ytimg.com/vi/Md-8YlE-KhA/hqdefault.jpg?custom=true&w=120&h=90&jpg444=true&jpgq=90&sp=68&sigh=LNUgfabMX-O17yvLiMnI5KZdEY4", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/Md-8YlE-KhA?list=PLtM4rw7sjPKUe0YW6Fmrp2FSU7RxkX-JA\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Booty Man")
+        
+        let p3 = PartyRock(imageURL: "https://i.ytimg.com/vi/tWyuglGCKgE/hqdefault.jpg?custom=true&w=120&h=90&jpg444=true&jpgq=90&sp=68&sigh=tZWE9Yflbx0HxzeO9ZZ3Zk51hHo", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/tWyuglGCKgE?list=PLtM4rw7sjPKUe0YW6Fmrp2FSU7RxkX-JA\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Juicy Wiggle")
+        
+        let p4 = PartyRock(imageURL: "https://i.ytimg.com/vi/xGiBiHocSZM/hqdefault.jpg?custom=true&w=120&h=90&jpg444=true&jpgq=90&sp=68&sigh=KQo7xeHPLfmlkrStyYz31AQw1kM", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/xGiBiHocSZM?list=PLtM4rw7sjPKUe0YW6Fmrp2FSU7RxkX-JA\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "New Thang")
+        
+        let p5 = PartyRock(imageURL: "https://i.ytimg.com/vi/1w9DiGlZksU/hqdefault.jpg?custom=true&w=120&h=90&jpg444=true&jpgq=90&sp=68&sigh=g1DTXpaVLQhtf9fTu3hAUMjKykI", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/1w9DiGlZksU?list=PLtM4rw7sjPKUe0YW6Fmrp2FSU7RxkX-JA\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Let's Get Ridiculous")
+        
+        partyRocks.append(p1)
+        partyRocks.append(p2)
+        partyRocks.append(p3)
+        partyRocks.append(p4)
+        partyRocks.append(p5)
+        
         tableView.delegate = self
         tableView.dataSource = self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
 extension ViewController: UITableViewDelegate {
@@ -43,15 +59,16 @@ extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        if let cell = tableView.dequeueReusableCell(withIdentifier: "PartyCell", for: IndexPath) as? PartyCell {
-            cell.updateUI(PartyRock: partyRocks)
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "PartyCell", for: indexPath) as? PartyTableViewCell {
+            let partyRock = partyRocks[indexPath.row]
+            cell.updateUI(partyRock: partyRock)
+            return cell
+        } else {
+            return UITableViewCell()
+            
         }
         
-        
-            
-            
-        }
-        return UITableViewCell()
     }
-    
 }
+
+
